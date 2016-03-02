b59d091b22492517082c015e26764fbd3a29e4c3
jottinger <C0nflict!>
2016-03-02 09:55:33 -0500

    Removing StringVesselParser, making VesselParser work

--- src/test/java/com/autumncode/javabot/grammar/VesselParser.java
+++ src/test/java/com/autumncode/javabot/grammar/VesselParser.java
@@ -4,11 +4,20 @@ import com.github.fge.grappa.parsers.BaseParser;
 import com.github.fge.grappa.rules.Rule;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class VesselParser extends BaseParser<Vessel> {
+    Collection<String> vessels= Stream.of(Vessel.values()).map(Enum::name).collect(Collectors.toList());
+
+    public Rule vesselType() {
+        return trieIgnoreCase(vessels);
+    }
+
     public Rule VESSEL() {
-        return firstOf("pint", "bowl");
+        return sequence(vesselType(),push(Vessel.valueOf(match().toUpperCase())));
     }
 }
